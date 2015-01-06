2f17585703fae3286367ed805b3eac07201d58df
Daniel Choi <dhchoi@gmail.com>
2015-01-06 12:23:34 -0500

    prettier Range construction; note Range is polymorphic, though Rating typed to Int

--- Main.hs
+++ Main.hs
@@ -20,7 +20,7 @@ data Constraint where
   LessThan :: (ToQVal a, Ord a) => Field a -> a -> Constraint 
   (:>:) :: (ToQVal a, Num a, Ord a) => Field a -> a -> Constraint
   Match :: Field String -> String -> Constraint 
-  Range :: (Show a, Num a) => Field a -> (a, a) -> Constraint 
+  Range :: (Show a, Num a) => Field a -> a -> a -> Constraint 
 
 -- for quoting in query if necessary
 class ToQVal a where
@@ -41,25 +41,27 @@ toQuery (Equals f v) = toDBField f ++ " == " ++ toQVal v
 toQuery (LessThan f v) = toDBField f ++ " < " ++ toQVal v
 toQuery (f :>: v) = toDBField f ++ " > " ++ toQVal v
 toQuery (Match f s) = toDBField f ++ " =~ " ++ toQVal s
-toQuery (Range f s@(x,y)) = toDBField f ++ " between " ++ show x ++ " and " ++ show y
+toQuery (Range f x y) = toDBField f ++ " between " ++ show x ++ " and " ++ show y
 
 fromPairs :: (String, [String]) -> Constraint
 fromPairs ("year",[x]) = Equals Year (read x)
-fromPairs ("year",x:y:_) = Range Year ((read x), (read y))
+fromPairs ("year",x:y:_) = Range Year (read x) (read y)
 fromPairs ("studio",[x]) = Equals Studio x
 fromPairs ("rating",[x]) = Rating :>: (read x)
+fromPairs ("rating",x:y:_) = Range Rating (read x) (read y)
 fromPairs _ = undefined
 
 main = do
   let c = Equals Year 1999
   -- let d = Match Studio 11 -- FAILS
   let d = Match Studio "Paramount" 
-  print $ map toQuery [Equals Year 1999, Match Studio "MGM", Range Rating (1,4)]
+  print $ map toQuery [Equals Year 1999, Match Studio "MGM", Range Rating 1 4]
   mapM_ print $ map (toQuery . fromPairs) [("year", ["2000"]), ("studio", ["Miramax"])]
   -- "year == 2000"
   -- "studio == \"Miramax\""
   mapM_ print $ map (toQuery . fromPairs) [("year", ["2000", "2002"])]
   mapM_ print $ map (toQuery . fromPairs) [("rating", ["3"])]
+  mapM_ print $ map (toQuery . fromPairs) [("rating", ["3", "5"])]
   -- "year between (2000,2002)"
 
   return ()
